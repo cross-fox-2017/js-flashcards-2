6d979ea48ec99001e6f80bf2a1d66912242b8613
Raditya <arya.raditya26@gmail.com>
2017-01-15 19:11:12 +0700

    done : MVC version

--- model.js
+++ model.js
@@ -1,2 +1,16 @@
 "use strict"
 // write your code here
+const fs     = require('fs')
+let argv     = process.argv
+
+class Model {
+  constructor(deck) {
+    this.deck = deck || 'music.json'
+  }
+
+  getData(){
+    return JSON.parse( fs.readFileSync( this.deck, 'UTF-8'))
+  }
+}
+
+module.exports = Model
